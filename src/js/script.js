9e160ff98e3b96fefa92af2ee0142f2b65ef7149
draket77 <94173365+draket77@users.noreply.github.com>
2026-04-29 22:40:25 +1000

    increase timeout

--- src/js/script.js
+++ src/js/script.js
@@ -243,7 +243,7 @@ function initPagesDropdown() {
     window.addEventListener('pageshow', () => {
         menu.style.display = '';
         close();
-        setTimeout(() => btn.blur(), 50);
+        setTimeout(() => btn.blur(), 500);
     });
 }
 
